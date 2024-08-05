e1f921dc80712477648017e0cd2da340c58b20a9
Jonathan <jonatan.ortega@cpic.cr>
2024-08-05 17:09:23 -0600

    Add Routing Children

--- src/app/authorize/authorize-routing.module.ts
+++ src/app/authorize/authorize-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
   {
     path:"",
     component: LayoutPageComponent
+  },{
+    path:"login",
+    component: LoginPageComponent
+  },
+  {
+    path:"register",
+    component: RegisterPageComponent
   }
 
 
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
 const routes: Routes = [
   {
     path:"",
     component: LayoutPageComponent
-  }
+  },
+  {
+    path:"hero",
+    component: HeroPageComponent
+  },
+  {
+    path:"search",
+    component: SearchPageComponent
+  },
+  {
+    path:"list",
+    component: ListPageComponent
+  },
+  {
+    path:"newhero",
+    component: NewPageComponent
+  },
+  
 ];
 
 @NgModule({
