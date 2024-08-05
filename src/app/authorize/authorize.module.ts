9a8d4b6b1eaac81f9978bc04b1458574475b735d
Jonathan <jonatan.ortega@cpic.cr>
2024-08-04 18:53:18 -0600

    Add Folder and Modules

---
+++ src/app/authorize/authorize.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthorizeRoutingModule } from './authorize-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthorizeRoutingModule
+  ]
+})
+export class AuthorizeModule { }
